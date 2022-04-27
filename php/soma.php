b0444a3e3b13af3451ecb6c9550a0ab64167f332
AmiltomAlexandre <99915613+AmiltomAlexandre@users.noreply.github.com>
2022-04-26 22:00:52 -0300

    first commit

---
+++ php/soma.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero1 = GET['numero1'];
+$numero1 = GET['numero2'];
+$soma = $numero1 + $numero2;
+print "<h2> Resultado = ".
+$soma."</h2>";
+
+
+?>
---
+++ php/subtração.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero1 = GET['numero1'];
+$numero1 = GET['numero2'];
+$subtração = $numero1 - $numero2;
+print "<h2> Resultado = ".
+$subtração."</h2>";
+
+
+?>
